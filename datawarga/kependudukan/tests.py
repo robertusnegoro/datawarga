ad119a9a8d3203424c044318e68f5a3142babb0a
Robertus Negoro <robeevanjava@gmail.com>
2022-12-18 19:34:15 +0700

    Add userlogin

--- datawarga/kependudukan/tests.py
+++ datawarga/kependudukan/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Warga
+from django.contrib.auth.models import User
 from datetime import datetime
 
 # Create your tests here.
@@ -9,6 +10,9 @@ class WargaTestCase(TestCase):
     def setUp(self):
         self.test_nama_lengkap = "TestCase1"
         self.test_nik = "nik_test_case1"
+        self.test_user = "testuser"
+        self.test_pass = "testpass123321123"
+        self.user = User.objects.create_user(username=self.test_user, password=self.test_pass)
         Warga.objects.create(nama_lengkap=self.test_nama_lengkap, nik=self.test_nik)
 
     def test_select_warga(self):
