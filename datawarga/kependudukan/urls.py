4fa9c2ed809016ed8f4cf174ee8d708700d829c6
Robertus Negoro <895404+robertusnegoro@users.noreply.github.com>
2023-01-12 19:18:48 +0700

    Add url prefix

--- datawarga/kependudukan/urls.py
+++ datawarga/kependudukan/urls.py
@@ -7,24 +7,25 @@ app_name = "kependudukan"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("form-warga/<int:idwarga>", views.formWarga, name="formWarga"),
+    path("warga/form-warga/<int:idwarga>", views.formWarga, name="formWarga"),
     path(
-        "delete-form-warga/<int:idwarga>", views.deleteFormWarga, name="deleteformWarga"
+        "warga/delete-form-warga/<int:idwarga>", views.deleteFormWarga, name="deleteformWarga"
     ),
-    path("form-warga-simpan", views.formWargaSimpan, name="formWargaSimpan"),
-    path("list-warga-view", views.WargaListView.as_view(), name="listWargaView"),
-    path("test-view", views.testView, name="testView"),
-    path("data-warga-pdf", views.listWargaReport, name="dataWargaPDF"),
-    path("dashboard-report", views.dashboard_warga, name="dashboardWarga"),
-    path("form-kompleks", views.kompleks_form, name="kompleksForm"),
-    path("generate-kompleks-exec", views.generate_kompleks, name="generateKompleks"),
+    path("warga/form-warga-simpan", views.formWargaSimpan, name="formWargaSimpan"),
+    path("warga/list-warga-view", views.WargaListView.as_view(), name="listWargaView"),
+    path("warga/test-view", views.testView, name="testView"),
+    path("warga/data-warga-pdf", views.listWargaReport, name="dataWargaPDF"),
+    path("warga/dashboard-report", views.dashboard_warga, name="dashboardWarga"),
+    path("warga/form-kompleks", views.kompleks_form, name="kompleksForm"),
+    path("warga/generate-kompleks-exec", views.generate_kompleks, name="generateKompleks"),
     path(
-        "list-kompleks-view", views.KompleksListView.as_view(), name="listKompleksView"
+        "warga/list-kompleks-view", views.KompleksListView.as_view(), name="listKompleksView"
     ),
-    path("delete-blok-form", views.delete_blok_form, name="deleteBlokForm"),
+    path("warga/delete-blok-form", views.delete_blok_form, name="deleteBlokForm"),
     path(
-        "detail-kompleks/<int:idkompleks>", views.detail_kompleks, name="detailKompleks"
+        "warga/detail-kompleks/<int:idkompleks>", views.detail_kompleks, name="detailKompleks"
     ),
+    path("warga/warga-rumah/<int:idkompleks>", views.warga_rumah, name="wargaRumah"),
     re_path(
         r"^%s(?P<path>.*)$" % settings.MEDIA_URL[1:],
         views.protected_serve,
